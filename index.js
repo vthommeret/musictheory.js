4ffb9fc6ef348e71ce311652593f16e259b209b2
Vernon Thommeret <synotic@gmail.com>
2018-12-21 01:27:44 -0800

    move musictheory entry file up

--- index.js
+++ index.js
@@ -1,36 +1,5 @@
-const mt = require('./lib/musictheory.js');
-
-// Usage
-
-const interval = mt.interval('m2');
-interval.print();
-
-const note = mt.note('C');
-note.print();
-
-const note2 = note.add(interval);
-note2.print();
-
-/*
-const note3 = note2.sub(interval);
-note3.print();
-*/
-
-/*
-const M3 = mt.interval('M3');
-const m3 = mt.interval('m3');
-
-const root = mt.note('C');
-root.print();
-
-const third = root.add(M3);
-third.print();
-
-const fifth = third.add(m3);
-fifth.print();
-*/
-
-/*
-const chord = mt.chord('C7');
-chord.print();
-*/
+module.exports = {
+  note: require('./lib/note.js'),
+  interval: require('./lib/interval.js'),
+  chord: require('./lib/chord.js'),
+};
