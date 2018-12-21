81b95f96168c5ae81ddfaffd27c26b77f18d8f7e
Vernon Thommeret <synotic@gmail.com>
2018-12-21 12:09:48 -0800

    expose library classes directly; move constructor methods to index.js; pseudo private field convention

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+const Note = require('./lib/note.js');
+const Interval = require('./lib/interval.js');
+const Chord = require('./lib/chord.js');
+
 module.exports = {
-  note: require('./lib/note.js'),
-  interval: require('./lib/interval.js'),
-  chord: require('./lib/chord.js'),
+  Note: Note,
+  note: function (val, octave, quality) {
+    return new Note(val, octave, quality);
+  },
+
+  Interval: Interval,
+  interval: function () {
+    return new interval.Interval(...arguments);
+  },
+
+  Chord: Chord,
+  chord: function (val) {
+    return new chord.Chord(val);
+  },
 };
